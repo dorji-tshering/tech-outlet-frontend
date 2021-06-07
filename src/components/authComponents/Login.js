cc0f024593a9a4693402e961ad85017877084b1c
DORJI <dorjitshering370@gmail.com>
2021-06-07 13:39:13 -0800

    css modules

--- src/components/authComponents/Login.js
+++ src/components/authComponents/Login.js
@@ -1,31 +1,25 @@
-import { NavLink } from 'react-router-dom';
-import { MdCopyright } from 'react-icons/md';
-import '../../styles/authStyles/login.css';
+import { Link, NavLink } from 'react-router-dom';
+import styles from '../../styles/authStyles/Login.module.css';
 
 const Login = () => {
-    
+     
         return (
-		<div className="login-container">
-			<h1 className="welcome-header">Welcome to E-Commerce!</h1>
-			<p className="welcome-para">Experience next generation shopping experience.</p>
-			<form action="" method="post" className="login-form">
+		<div className={styles.login_container}>
+			<h1 className={styles.welcome_header}>Welcome to E-Commerce!</h1>
+			<p className={styles.welcome_para}>Experience next generation shopping experience.</p>
+			<form action="" method="post" className={styles.login_form}>
 			
-				<label htmlFor="phone" className="form-label">Mobile Number</label>
-				<input type="tel" name="phone" className="form-input" placeholder="Mobile number" />
+				<label htmlFor="phone" className={styles.form_label}>Mobile Number</label>
+				<input type="tel" name="phone" className={styles.form_input} placeholder="Mobile number" />
 			
-				<label htmlFor="password" className="form-label">Password</label>
-				<input type="password" name="password" className="form-input" placeholder="Password"/>
+				<label htmlFor="password" className={styles.form_label}>Password</label>
+				<input type="password" name="password" className={styles.form_input} placeholder="Password"/>
 
-				<NavLink to="" className="forgot-password">Forgot Password?</NavLink>				
-				<button type="submit" className="submit-button">LOGIN</button>
+				<NavLink to="" className={styles.forgot_password}>Forgot Password?</NavLink>				
+				<button type="submit" className={styles.submit_button}>LOGIN</button>
 
 			</form>
-			<section className="link-to-signup">
-				<h3 className="new-to-ecom">New To E-Commerce?</h3>
-				<NavLink to="/signup" className="signup-link">SIGNUP HERE!</NavLink>
-			</section>
-		
-			<p className="copyright"><MdCopyright className="c-icon" /> 2021, @TIKU</p>
+			<p>New to U_T_O?  <Link to="/signup">Signup here</Link></p>
 		</div>
         )
 }
--- src/components/authComponents/Signup.js
+++ src/components/authComponents/Signup.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
-import '../../styles/authStyles/signup.css';
+import { Link } from 'react-router-dom';
+import styles from '../../styles/authStyles/Signup.module.css';
 
 const Signup = () => {
 
 	return (
-		<div className="sign-in-container">
-			signup
+		<div className={styles.signup_container}>
+			<h1 className={styles.welcome_header}>Welcome to T_U_O!</h1>
+			<p className={styles.welcome_para}>Experience next generation shopping experience.</p>
+			
+			<form action="" method="POST" className={styles.signup_form}>				
+				<label htmlFor="name" className={styles.form_label}>Mobile Number</label>
+				<input type="text" name="name" className={styles.form_input} placeholder="Full name" />
+
+				<label htmlFor="phone" className={styles.form_label}>Mobile Number</label>
+				<input type="tel" name="phone" className={styles.form_input} placeholder="Mobile number" />
+
+				<label htmlFor="email" className={styles.form_label}>Mobile Number</label>
+				<input type="email" name="email" className={styles.form_input} placeholder="Email" />
+
+				<label htmlFor="password" className={styles.form_label}>Mobile Number</label>
+				<input type="password" name="password" className={styles.form_input} placeholder="Password" />
+
+				<button type="submit" className={styles.submit_button}>Create Account</button>		
+			</form>
+			<p>Already have an accout?  <Link to="/login">Signin here</Link></p>
 		</div>
 	)
 }
